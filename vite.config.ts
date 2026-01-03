e95c24f116178f4673ab49234e0a505675c968b6
gabriel <gabriel.dait@gmail.com>
2026-01-03 15:05:07 -0300

    Build and deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fechaconta/",
+  base: mode === "production" ? "/fechaconta/" : "/",
   server: {
     host: "::",
     port: 8080,
